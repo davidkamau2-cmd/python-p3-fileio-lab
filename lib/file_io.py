df9d091cbafe97c52c7f962c9914e5c246bb9b18
David <kabathi98@gmail.com>
2025-12-10 12:08:38 +0300

    finished the lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,27 @@
 def write_file(file_name, file_content):
-    pass
+    """Write `file_content` to `file_name`.txt.
+
+    `file_name` may be a string or a Path-like object. The function
+    ensures the file is opened with a `.txt` extension.
+    """
+    path = str(file_name) + ".txt"
+    with open(path, 'w') as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Append `append_content` to `file_name`.txt.
+
+    `file_name` may be a string or a Path-like object.
+    """
+    path = str(file_name) + ".txt"
+    with open(path, 'a') as text_file:
+        text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Read and return the contents of `file_name`.txt.
+
+    `file_name` may be a string or a Path-like object.
+    """
+    path = str(file_name) + ".txt"
+    with open(path, 'r') as text_file:
+        return text_file.read()
